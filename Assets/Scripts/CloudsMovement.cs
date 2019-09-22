810cf8e085ed0c9d90458a2d1953ebd65f2a7335
Samuel Garrido <samuel.garridor@utadeo.edu.co>
2019-09-22 11:17:05 -0500

    .

---
+++ Assets/Scripts/CloudsMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudsMovement : MonoBehaviour { 
+
+    public float Speed = 10f;
+    private Rigidbody2D rb;
+    private Vector2 screenBounds; 
+   
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(-Speed,0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+    void Update()
+    {
+        if (transform.position.x < screenBounds.x * -2) {
+
+            Destroy(this.gameObject);
+
+        }
+    
+
+}
+
+
+    
+}
---
+++ Assets/Scripts/deployClouds.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deployClouds : MonoBehaviour
+{
+    public GameObject cloudPrefab;
+    public float respawnTime = 1.0f;
+    private Vector2 screenBounds;
+    void Start()
+    {
+
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        StartCoroutine(CloudWave());
+
+    }
+
+    private void spwanCloud() {
+        GameObject a = Instantiate(cloudPrefab) as GameObject;
+        a.transform.position = new Vector2(screenBounds.x * 2, Random.Range(-screenBounds.y, screenBounds.y));
+    
+        
+    }
+
+    IEnumerator CloudWave() {
+        while (true) {
+            yield return new WaitForSeconds(respawnTime);
+            spwanCloud();
+        }
+    }
+}
